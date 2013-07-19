7c00ca9a81ed8086a76e573c9ff86e6d02646241
Jeremy Freehill <jfreehill@gmail.com>
2013-07-19 17:06:40 -0500

    added close icon and styles for portfolio page

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,3 +1,17 @@
 ;
 
-//$('nav a').tooltip();
+$('.info-button').on('click', toggleInfo);
+
+function toggleInfo(){
+	var _this = this;
+	$(_this).siblings('.info').add(_this).toggleClass('active');
+	var icon = $(_this).children('i').eq(0);
+	var iconClass =  (icon.hasClass('icon-info-circled')) ? 'icon-cancel' : 'icon-info-circled';
+	icon.removeClass().addClass(iconClass);
+
+	// if ($(_this).hasClass('active')) {
+	// 	setTimeout(function(){
+	// 		toggleInfo.apply(_this);
+	// 	} , 4000);
+	// }
+}
