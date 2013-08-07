612fe3baf2e0fe973d234775e92b31b765acff48
Jeremy Freehill <jfreehill@gmail.com>
2013-08-07 17:19:22 -0500

    adjusted line spacing

--- assets/data/portfolio.js
+++ assets/data/portfolio.js
@@ -1,85 +1,85 @@
 portfolio = (window.Portfolio) ? new Portfolio() : function Portfolio(){};
 
 portfolio.data = 
-	{
-		"entries": [
-			{
-				"client": "Jim Beam Honey",
-				"project": "Experiential",
-				"role": "Full Development",
-				"thumb": "beam_honey.jpg",
-				"link": "http://jimbeamhoneybear.com"
-			},
-			{
-				"client": "Pinnacle Vodka",
-				"project": "Brand Site",
-				"role": "Full Development",
-				"thumb": "pinnacle_brand.jpg",
-				"link": "http://pinnaclevodka.com"
-			},
-			{
-				"client": "Pinnacle Vodka",
-				"project": "Recipe Finder",
-				"role": "Full Development",
-				"thumb": "pinnacle_recipes.jpg",
-				"link": "http://fb.pinnaclevodka.com/recipes-new/"
-			},
-			{
-				"client": "CDW",
-				"project": "Solutions Blog",
-				"role": "Full Development",
-				"thumb": "cdw_solutionsblog.jpg",
-				"link": "http://cdwsolutionsblog.com/"
-			},
-			{
-				"client": "Ashford University",
-				"project": "Cloud Campus",
-				"role": "Front-End Development",
-				"thumb": "ashford_parallax.jpg",
-				"link": "http://bright.ashford.edu/"
-			},
-			{
-				"client": "Ashford University",
-				"project": "Belong",
-				"role": "Full Development",
-				"thumb": "ashford_simple.jpg",
-				"link": "http://belong.ashford.edu/"
-			},
-			{
-				"client": "Pull-Ups",
-				"project": "iPad Memory Game",
-				"role": "Full Development",
-				"thumb": "pullups_memorygame.jpg",
-				"link": false
-			},
-			{
-				"client": "JMK Remodeling",
-				"project": "Home Page",
-				"role": "Design & Development",
-				"thumb": "jmkremodeling.jpg",
-				"link": "http://jmkremodelers.com"
-			},
-			{
-				"client": "LR Hein Construction",
-				"project": "Home Page",
-				"role": "Design & Development",
-				"thumb": "lrhein.jpg",
-				"link": "http://lrhein.com"
-			},
-			{
-				"client": "Roman",
-				"project": "Home Page",
-				"role": "Full Development",
-				"thumb": "roman_brand.jpg",
-				"link": "http://roman.com"
-			},
-			{
-				"client": "Gruntwork Game Site",
-				"project": "Home Page",
-				"role": "Design & Development",
-				"thumb": "gruntwork.jpg",
-				"link": "http://game.colum.edu/projects/GruntWork/"
-			}
-		]
-	}
-	// end data
+{
+	"entries": [
+		{
+			"client": "Jim Beam Honey",
+			"project": "Experiential",
+			"role": "Full Development",
+			"thumb": "beam_honey.jpg",
+			"link": "http://jimbeamhoneybear.com"
+		},
+		{
+			"client": "Pinnacle Vodka",
+			"project": "Brand Site",
+			"role": "Full Development",
+			"thumb": "pinnacle_brand.jpg",
+			"link": "http://pinnaclevodka.com"
+		},
+		{
+			"client": "Pinnacle Vodka",
+			"project": "Recipe Finder",
+			"role": "Full Development",
+			"thumb": "pinnacle_recipes.jpg",
+			"link": "http://fb.pinnaclevodka.com/recipes-new/"
+		},
+		{
+			"client": "CDW",
+			"project": "Solutions Blog",
+			"role": "Full Development",
+			"thumb": "cdw_solutionsblog.jpg",
+			"link": "http://cdwsolutionsblog.com/"
+		},
+		{
+			"client": "Ashford University",
+			"project": "Cloud Campus",
+			"role": "Front-End Development",
+			"thumb": "ashford_parallax.jpg",
+			"link": "http://bright.ashford.edu/"
+		},
+		{
+			"client": "Ashford University",
+			"project": "Belong",
+			"role": "Full Development",
+			"thumb": "ashford_simple.jpg",
+			"link": "http://belong.ashford.edu/"
+		},
+		{
+			"client": "Pull-Ups",
+			"project": "iPad Memory Game",
+			"role": "Full Development",
+			"thumb": "pullups_memorygame.jpg",
+			"link": false
+		},
+		{
+			"client": "JMK Remodeling",
+			"project": "Home Page",
+			"role": "Design & Development",
+			"thumb": "jmkremodeling.jpg",
+			"link": "http://jmkremodelers.com"
+		},
+		{
+			"client": "LR Hein Construction",
+			"project": "Home Page",
+			"role": "Design & Development",
+			"thumb": "lrhein.jpg",
+			"link": "http://lrhein.com"
+		},
+		{
+			"client": "Roman",
+			"project": "Home Page",
+			"role": "Full Development",
+			"thumb": "roman_brand.jpg",
+			"link": "http://roman.com"
+		},
+		{
+			"client": "Gruntwork Game Site",
+			"project": "Home Page",
+			"role": "Design & Development",
+			"thumb": "gruntwork.jpg",
+			"link": "http://game.colum.edu/projects/GruntWork/"
+		}
+	]
+}
+// end data
