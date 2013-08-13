6c108f29d53397fb0e990b0c2ff31219d801b876
Jeremy Freehill <jfreehill@gmail.com>
2013-08-13 18:11:57 -0500

    mostly converted portfolio to a node/express project

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+
+/*
+ * GET home page.
+ */
+
+// exports.index = function(req, res){
+//   res.render('index', { title: 'Express' });
+// };
+
+exports.index = function(req, res){
+	res.render('index', {page: "home", title: "Web Designer & Developer"});
+};
+
+exports.portfolio = function(req, res){
+	var fs = require('fs');
+	var file ='./data/portfolio.json';
+	var jsonData;
+
+	fs.readFile(file, 'utf8', function(err, data){
+		if (err) { 
+			console.log('Error: ' + err); 
+			return;
+		}
+		jsonData = JSON.parse(data);
+		res.render('portfolio', {data: jsonData, page: "portfolio", title: "Portfolio"});
+	});
+};
+
+exports.lab = function(req, res){
+	res.sendfile('./views/lab.htm');
+};
