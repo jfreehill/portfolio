e3185d351dbee38d09d2617f36476509a8ae3032
Jeremy Freehill <jfreehill@gmail.com>
2013-09-03 12:24:08 -0500

    Reverted behavior to previous version

--- public/assets/js/behavior.js
+++ public/assets/js/behavior.js
@@ -1,52 +1,43 @@
 ;
 
-var Q = function(query) { 
-	return document.querySelectorAll(query);
-}
+define(['zepto'], function(){
 
-Q.prototype.hasClass = function(classToFind) {
-	var el = this;
-	return (el[0].getAttribute('class').indexOf(classToFind) > 0);
-}
+	var lines = 5;
+	for (n = 0; n < lines; n++) {
+		var line = $('<div>').attr('class', 'line-'+(n+1));
+		$('header').eq(0).prepend(line);
+	}
 
-var lines = 5;
-for (n = 0; n < lines; n++) {
-	var line = $('<div>').attr('class', 'line-'+(n+1));
-	$('header').eq(0).prepend(line);
-}
+	if ($('body').hasClass('portfolio')) {
+		viewRender();
+	}
 
-if ($('body').hasClass('portfolio')) {
-	randomizeColors(3);
-}
+	function viewRender(){
+		$('.entry .info').each(function(){
+			var n;
+			function rand(){	
+				var r = Math.round(Math.random()*3);
+				if (r < 1) r++;
+				return r;
+			}
+			n = rand();
+			$(this).addClass('color-'+n);
+		});
+		$('.info-button').on('click', toggleInfo);
+	}
 
-// apply random color classes to thumbnails
-$.fn.randomizeColors = function(num){
-	this.each(function(){
-		var n;
-		function rand(){	
-			var r = Math.round(Math.random()*num);
-			if (r < 1) r++;
-			return r;
-		}
-		n = rand();
-		$(this).addClass('color-'+n);
+	function toggleInfo(){
+		var _this = this;
+		$(_this).siblings('.info').add(_this).toggleClass('active');
+		var icon = $(_this).children('i').eq(0);
+		icon.toggleClass('icon-cancel icon-info-circled');
+	}
+
+	$('.open-menu').on('click', function(e){
+		e.preventDefault();
+		$(this).toggleClass('active');
+		$('.wrap').toggleClass('open');
+		$(this).children('i').eq(0).toggleClass('icon-menu icon-angle-circled-up');
 	});
-}
-
-function toggleInfo(){
-	var _this = this;
-	$(_this).siblings('.info').add(_this).toggleClass('active');
-	var icon = $(_this).children('i').eq(0);
-	icon.toggleClass('icon-cancel icon-info-circled');
-}
-
-$('.info-button').on('click', toggleInfo);
-
-$('.open-menu').on('click', function(e){
-	e.preventDefault();
-	$(this).toggleClass('active');
-	$('.wrap').toggleClass('open');
-	$(this).children('i').eq(0).toggleClass('icon-menu icon-angle-circled-up');
+	
 });
-
-
