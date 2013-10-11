a6e0df22e6bcc193588a177740005c285a50c13c
Jeremy Freehill <jfreehill@gmail.com>
2013-10-11 11:58:02 -0500

    Metal mode now in full effect.

--- public/js/behavior.js
+++ public/js/behavior.js
@@ -37,4 +37,22 @@
 		$(this).children('i').eq(0).toggleClass('icon-menu icon-angle-circled-up');
 	});	
 
+	/* jQuery Contra v1.0
+	 * http://jonraasch.com/blog/jquery-contra-plugin
+	 *
+	 * Copyright (c) 2009 Jon Raasch (http://jonraasch.com/)
+	 * Licensed under the MIT License (see http://dev.jonraasch.com/contra/docs#licensing)
+	 */
+	(function(a){a.contra=function(e,d){function c(f){if(f==d.code[b]){b++;if(b>=d.code.length){e()}}else{b=0}}if(typeof e!="function"){return false}var b=0,d=d||{};d.scope=d.scope||a(document);d.code=d.code||[38,38,40,40,37,39,37,39,66,65,13];d.scope.keyup(function(f){c(f.which||f.keyCode)})};a.fn.contra=function(c,b){var b=b||{};b.scope=a(this);a.contra(c,b)}})(window.jQuery || window.Zepto);
+	$.contra(function(){
+		$('html').toggleClass('EXTREME');
+	}, {
+		code: [
+			77,
+			69,
+			84,
+			65,
+			76
+		]
+	});
 });
